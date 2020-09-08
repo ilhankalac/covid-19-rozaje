8716cd6fbd6e76733ebd1a0ae20bc4e4d4869361
ilhankalac <ilhandeveloper@gmail.com>
2020-09-08 14:32:30 +0200

    starting project configuration

---
+++ covid19rozaje/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartComponent } from './chart/chart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
